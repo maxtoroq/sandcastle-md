05c98a8e14ad1eca8a8d14167f079571e10aa1e6
Max Toro <maxtoroq@gmail.com>
2014-04-26 16:17:14 -0300

    resolve arguments against current directory

--- src/sandcastle-md/Program.cs
+++ src/sandcastle-md/Program.cs
@@ -16,16 +16,33 @@ namespace sandcastle_md {
 
          var compiler = proc.NewXsltCompiler();
 
+         string currentDir = Environment.CurrentDirectory;
+         string inputDir = args[0];
+         string outputDir = args[1];
+
+         if (currentDir.Last() != Path.DirectorySeparatorChar) {
+            currentDir += Path.DirectorySeparatorChar;
+         }
+
+         if (inputDir.Last() != Path.DirectorySeparatorChar) {
+            inputDir += Path.DirectorySeparatorChar;
+         }
+
+         if (outputDir.Last() != Path.DirectorySeparatorChar) {
+            outputDir += Path.DirectorySeparatorChar;
+         }
+
          var baseUri = new Uri(AppDomain.CurrentDomain.BaseDirectory, UriKind.Absolute);
+         var callerBaseUri = new Uri(currentDir, UriKind.Absolute);
 
          var exec = compiler.Compile(new Uri(baseUri, "sandcastle-md-all.xsl"));
 
          var transformer = exec.Load();
          transformer.InitialTemplate = new QName("main");
-         transformer.SetParameter(new QName("source-dir"), new XdmAtomicValue(new Uri(baseUri, args[0])));
+         transformer.SetParameter(new QName("source-dir"), new XdmAtomicValue(new Uri(callerBaseUri, inputDir)));
 
          if (args.Length > 1) {
-            transformer.SetParameter(new QName("output-dir"), new XdmAtomicValue(new Uri(baseUri, args[1])));
+            transformer.SetParameter(new QName("output-dir"), new XdmAtomicValue(new Uri(callerBaseUri, outputDir)));
          }
 
          var serializer = new Serializer();
