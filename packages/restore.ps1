390d1218a7cfa8e2eae07b6cc98e6b6ba7227a04
Max Toro <maxtoroq@gmail.com>
2025-08-30 20:39:12 -0400

    target .net v4.7.2 and updated restore script

--- packages/restore.ps1
+++ packages/restore.ps1
@@ -1,26 +1,8 @@
-﻿
-$ErrorActionPreference = "Stop"
+﻿$ErrorActionPreference = "Stop"
 Push-Location (Split-Path $script:MyInvocation.MyCommand.Path)
 
-$nuget = "..\.nuget\nuget.exe"
-
-function ensure-nuget {
-   if (-not (Test-Path $nuget -PathType Leaf)) {
-      
-      $nuget_dir = Split-Path -Parent $nuget
-
-      if (-not (Test-Path $nuget_dir -PathType Container)) {
-         md $nuget_dir | Out-Null
-      }
-
-      write "Downloading NuGet..."
-      Invoke-WebRequest https://www.nuget.org/nuget.exe -OutFile $nuget
-   }
-}
-
 function main {
-   ensure-nuget
-   &$nuget restore ..\sandcastle-md.sln
+   MSBuild ..\sandcastle-md.sln -t:Restore -p:RestorePackagesConfig=true
 }
 
 try {
